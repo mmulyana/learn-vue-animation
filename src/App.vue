e66d90c96a148fd0178254906c359f06d0710eec
mmulyana <mulyan.t20@gmail.com>
2025-09-15 15:28:14 +0700

    feat: transition component

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const showText = ref(false)
+</script>
+
+<template>
+	<Transition name="fade">
+		<h1 v-if="showText">Hello world</h1>
+	</Transition>
+	<button @click="showText = !showText">Toggle</button>
+</template>
+
+<style scoped>
+.fade-enter-from {
+	opacity: 0;
+}
+
+.fade-enter-to {
+	opacity: 1;
+}
+
+.fade-enter-active {
+	transition: all 1s ease;
+}
+</style>
