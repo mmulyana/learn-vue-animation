9b514658a1a7707899a720d7527d3ab99946f227
mmulyana <mulyan.t20@gmail.com>
2025-09-15 15:48:19 +0700

    feat: todo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,98 @@
 <script setup>
 import { ref } from 'vue'
 
-const showText = ref(false)
+const newTodo = ref('')
+const todos = ref([
+	{ id: 1, text: 'Belajar Vue 3' },
+	{ id: 2, text: 'Bikin CRUD Todo' },
+])
+
+function addTodo() {
+	if (!newTodo.value) return
+	todos.value.unshift({
+		id: Date.now(),
+		text: newTodo.value,
+	})
+	newTodo.value = ''
+}
+
+function removeTodo(id) {
+	todos.value = todos.value.filter((t) => t.id !== id)
+}
 </script>
 
 <template>
-	<Transition name="fade">
-		<h1 v-if="showText">Hello world</h1>
-	</Transition>
-	<button @click="showText = !showText">Toggle</button>
+	<div class="todo-app">
+		<div class="todo-form">
+			<input
+				v-model="newTodo"
+				placeholder="Tambah todo..."
+				@keyup.enter="addTodo"
+				class="todo-input"
+			/>
+			<button @click="addTodo">Tambah</button>
+		</div>
+
+		<TransitionGroup name="slide" tag="ul" class="todo-list">
+			<li v-for="todo in todos" :key="todo.id" class="todo-item">
+				{{ todo.text }}
+				<button @click="removeTodo(todo.id)">❌</button>
+			</li>
+		</TransitionGroup>
+	</div>
 </template>
 
 <style scoped>
-.fade-enter-from {
-	opacity: 0;
+.todo-list {
+	position: relative;
+	list-style: none;
+	padding: 0;
+	transition: all 5s ease;
 }
 
-.fade-enter-to {
+.todo-item {
+	display: flex;
+	justify-content: space-between;
+	margin: 6px 0;
+	padding: 8px 12px;
+	border-radius: 8px;
+	position: relative;
+}
+
+.todo-input {
+	height: 32px;
+	border-radius: 8px;
+	border: none;
+  padding: 0 0.8em;
+}
+
+.todo-form {
+	display: flex;
+	gap: 1rem;
+	justify-content: space-between;
+}
+
+.slide-enter-from {
+	opacity: 0;
+	transform: translateY(-30px);
+	position: absolute;
+	width: 100%;
+}
+.slide-enter-to {
+	opacity: 1;
+	transform: translateY(0);
+}
+.slide-enter-active {
+	transition: all 0.4s ease;
+}
+
+.slide-leave-from {
 	opacity: 1;
 }
-
-.fade-enter-active {
-	transition: all 1s ease;
-}
-
-.fade-leave-from {
-	opacity: 1;
-}
-
-.fade-leave-to {
+.slide-leave-to {
 	opacity: 0;
 }
-
-.fade-leave-active {
-	transition: all 1s ease;
+.slide-leave-active {
+	transition: all 0.4s ease;
 }
 </style>
